5cd3af6813392b87c2fa1b777229838e5e556aa8
DarshilTheCoder <102150953+DarshilTheCoder@users.noreply.github.com>
2022-03-25 19:03:50 +0530

    Create university.js

---
+++ university.js
@@ -0,0 +1,9 @@
+var toggle = document.getElementById("nav_links");
+
+        function closeMenu(){
+            toggle.style.display = "none";
+        }
+        function openMenu(){
+            toggle.style.display = "block";
+        }
+
